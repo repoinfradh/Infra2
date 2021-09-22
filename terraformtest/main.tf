612e7b9f378f1ac8ac1200b134ce0c2e67b4a411
repoinfradh <88364639+repoinfradh@users.noreply.github.com>
2021-09-21 21:50:57 -0300

    Update main.tf

--- terraformtest/main.tf
+++ terraformtest/main.tf
@@ -8,18 +8,18 @@ terraform {
  }
 
  required_version = ">= 0.14.9"
-}
 
-provider "aws" {
- profile = "default"
- region  = "us-west-2"
-}
+  provider "aws" {
+    profile = "default"
+    region  = var.my_region_us_west_2
+  }
 
-resource "aws_instance" "app_server" {
- ami           = "ami-08d70e59c07c61a3a"
- instance_type = "t2.micro"
+  resource "aws_instance app_server" {
+    ami          := "ami-08d70e59c07c61a3a"
+    instance_type = "t2.micro"
 
- tags = {
-   Name = var.instance_name
- }
+    tags = {
+      Name = local.instance_name
+    }
+  }
 }
