ec235f7b81c1a5bb00682037d8c2fd7ea1e469e3
repoinfradh <88364639+repoinfradh@users.noreply.github.com>
2021-09-21 21:47:40 -0300

    Create main.tf

---
+++ terraformtest/main.tf
@@ -0,0 +1,33 @@
+# main.tf
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.27"
+   }
+ }
+
+ required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+ profile = "default"
+ region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+ ami           = "ami-08d70e59c07c61a3a"
+ instance_type = "t2.micro"
+
+ tags = {
+   Name = var.instance_name
+ }
+}
+
+
+# variables.tf
+variable "instance_name" {
+ description = "Value of the Name tag for the EC2 instance"
+ type        = string
+ default     = "ExampleAppServerInstance"
+}
