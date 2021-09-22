5db710fa26af9b31d0cabefaf3e56eb6ec0d7744
repoinfradh <88364639+repoinfradh@users.noreply.github.com>
2021-09-21 21:51:36 -0300

    Update variables.tf

--- terraformtest/variables.tf
+++ terraformtest/variables.tf
@@ -1,6 +1,6 @@
 # variables.tf
 variable "instance_name" {
- description = "Value of the Name tag for the EC2 instance"
+ description = Value of the Name tag for the EC2 instance
  type        = string
  default     = "ExampleAppServerInstance"
 }
